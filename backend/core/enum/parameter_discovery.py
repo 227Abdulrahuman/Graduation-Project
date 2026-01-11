c4b9cac167852e2d7d958f29d6654d0dacdb09c7
Abdulrahman <147337877+227Abdulrahuman@users.noreply.github.com>
2026-01-11 09:01:04 +0200

    Parameters Discovery (#56)
    * Finalizing Recon Module.
    
    * Adding Crawller worker.
    
    * Adding Parameter Discovery

---
+++ backend/core/enum/parameter_discovery.py
@@ -0,0 +1,43 @@
+import requests
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.api.settings")
+django.setup()
+from backend.core.models import *
+from backend.core.enum.utils import load_urls_for_mining, heuristic, parse_headers
+from urllib.parse import urlparse
+
+def parameter_extractor(url, auth_headers=None):
+    """
+    Takes a domain and extracts parameters from <input> and <script> and saves to the db.
+    """
+    #Load target URLs.
+    domain = urlparse(url).hostname
+    urls_file = f'/work/backend/core/enum/output/{domain}/htmlurls.txt'
+    load_urls_for_mining(domain)
+
+    params = list()
+    urls = set()
+
+    #Extract the parameters.
+    h = parse_headers(auth_headers)
+    h['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
+    with open(urls_file, 'r') as file:
+        for line in file:
+            line = line.strip()
+            urls.add(line)
+
+    for i in urls:
+        res = requests.get(i,headers=h)
+        p = heuristic(res)
+        params.extend(p)
+
+    #Save to the database
+    sub_obj = Subdomain.objects.get(hostname=domain)
+    for i in params:
+        Parameter.objects.update_or_create(
+            subdomain=sub_obj,
+            key=i,
+            defaults={
+                "value":"EXTRACTED",
+            }
+        )
